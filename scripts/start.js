4ffcc6d328589ab2efe2dd48ea4f999d297cbaa1
jinxiao <jinxiao@me.com>
2018-10-19 17:08:10 +0800

    fix tsconfig

--- scripts/start.js
+++ scripts/start.js
@@ -12,7 +12,7 @@ process.on('unhandledRejection', err => {
 });
 
 // Ensure environment variables are read.
-require('../config/env');
+require('../src/config/env');
 
 const fs = require('fs');
 const chalk = require('chalk');
@@ -27,9 +27,9 @@ const {
   prepareUrls,
 } = require('react-dev-utils/WebpackDevServerUtils');
 // const openBrowser = require('react-dev-utils/openBrowser');
-const paths = require('../config/paths');
-const config = require('../config/webpack.config.dev');
-const createDevServerConfig = require('../config/webpackDevServer.config');
+const paths = require('../src/config/paths');
+const config = require('../src/config/webpack.config.dev');
+const createDevServerConfig = require('../src/config/webpackDevServer.config');
 
 const useYarn = fs.existsSync(paths.yarnLockFile);
 const isInteractive = process.stdout.isTTY;
